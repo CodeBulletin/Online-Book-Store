4259d59cbb4b845ac1956efeba1fc16aa7a6c48b
CodeBulletin <CodeBulletin@github.com>
2022-02-05 19:53:28 +0530

    Adding HomePage and Libraries

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <script src="./Libraries/js/bootstrap.min.js"></script>
+        <script src="./Libraries/js/jquery-3.6.0.slim.min.js"></script>
+        <link rel="stylesheet" href="./Libraries/css/bootstrap.min.css">
+
+        <title>Home Page</title>
+    </head>
+    <body>
+        <?php
+            phpinfo();
+        ?>
+    </body>
+</html>
