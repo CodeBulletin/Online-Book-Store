89d0c394e7b6b0bda3fede09091a5fd5c45fe2a6
CodeBulletin <CodeBulletin@github.com>
2022-02-06 08:51:05 +0530

    Adding Main Files

--- index.php
+++ index.php
@@ -5,15 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <script src="./Libraries/js/bootstrap.min.js"></script>
-        <script src="./Libraries/js/jquery-3.6.0.slim.min.js"></script>
-        <link rel="stylesheet" href="./Libraries/css/bootstrap.min.css">
-
-        <title>Home Page</title>
+        <title>Index Page</title>
     </head>
     <body>
         <?php
-            phpinfo();
+            header("Location: ./PHP/HomePage.php");
         ?>
     </body>
 </html>
